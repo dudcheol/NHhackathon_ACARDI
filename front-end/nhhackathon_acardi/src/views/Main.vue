4a350863a12a9a046ed37ba4542c451fcdad9e07
KimYH-KU <kimyh03160@naver.com>
2020-12-10 17:10:34 +0900

    test

--- front-end/nhhackathon_acardi/src/views/Main.vue
+++ front-end/nhhackathon_acardi/src/views/Main.vue
@@ -4,6 +4,7 @@
     <router-link to="/main/calendar">Calendar</router-link> |
     <router-link to="/main/list">List</router-link> |
     <router-view></router-view>
+    <h1>test</h1>
   </div>
 </template>
 
