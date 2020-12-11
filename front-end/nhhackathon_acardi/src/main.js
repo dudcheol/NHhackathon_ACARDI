fcbd22eaf0cb40c7abd35302a350ba7bf6e88da3
dudcheol <95_pyc@naver.com>
2020-12-11 18:21:35 +0900

    병합충돌수정

--- front-end/nhhackathon_acardi/src/main.js
+++ front-end/nhhackathon_acardi/src/main.js
@@ -3,23 +3,17 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
-<<<<<<< HEAD
 import VueSession from 'vue-session';
+import VCalendar from 'v-calendar';
 
 var sessionOptions = {
-  persist: true
-}
+  persist: true,
+};
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 Vue.use(VueSession, sessionOptions);
-=======
-import VCalendar from 'v-calendar';
-
-Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
 Vue.use(VCalendar);
->>>>>>> main_vue
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
